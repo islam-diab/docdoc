48cd05079af54a5a1b82510e8ea20dcf37c27025
Islam Diab <islamdiab304@gmail.com>
2023-12-14 21:06:27 +0200

    add dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('hi'),
-        ),
-      ),
+      home: Scaffold(appBar: AppBar(title: Text('df'),),
     );
   }
 }
