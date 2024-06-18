ffd289d22a00e8690f558c0c4dcb74ec89dc69b6
Islam Diab <islamdiab304@gmail.com>
2024-06-19 02:49:44 +0300

    -Bumped version 1.0.2 -Added fastlane to Android for firebase distribution

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -2,10 +2,12 @@ import 'package:docdoc/core/di/dependecy_injection.dart';
 import 'package:docdoc/core/routing/app_router.dart';
 import 'package:docdoc/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 
-void main() {
+void main()async {
   setupGetIt();
+  await ScreenUtil.ensureScreenSize();
   runApp(
     DocApp(
       appRouter: AppRouter(),
