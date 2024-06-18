ba8df2ce59a0ee2b0de535acd7fb28b38f313077
Islam Diab <islamdiab304@gmail.com>
2024-06-18 17:11:28 +0300

    - add Development & Production flavor for Android -add two main files

---
+++ lib/main_production.dart
@@ -0,0 +1,14 @@
+import 'package:docdoc/core/di/dependecy_injection.dart';
+import 'package:docdoc/core/routing/app_router.dart';
+import 'package:docdoc/doc_app.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  setupGetIt();
+  runApp(
+    DocApp(
+      appRouter: AppRouter(),
+    ),
+  );
+}
