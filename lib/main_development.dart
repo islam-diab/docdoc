5181824d61af4252e33d065d7fb488b35e6318be
Islam Diab <islamdiab304@gmail.com>
2024-06-19 16:44:46 +0300

    test test

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -4,7 +4,7 @@ import 'package:docdoc/doc_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-
+// dart run build_runner build
 void main() async {
   setupGetIt();
   await ScreenUtil.ensureScreenSize();
